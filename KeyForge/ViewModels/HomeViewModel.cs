f6a1536a1747b237b6965d9309da7a14f947843f
GitSchwan <fabianschwanck@hotmail.com>
2026-03-10 16:04:51 +0100

    Update HomeViewModel and HomeView: introduce `ObservableCollection` for site data and update grid layout in HomeView

--- KeyForge/ViewModels/HomeViewModel.cs
+++ KeyForge/ViewModels/HomeViewModel.cs
@@ -1,5 +1,34 @@
-﻿namespace KeyForge.ViewModels;
+﻿
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace KeyForge.ViewModels;
 
 public class HomeViewModel : ViewModelBase
 {
+    public ObservableCollection<Sites> Data { get; }
+
+    public HomeViewModel()
+    {
+        var data = new List<Sites>
+        {
+            new Sites("youtube.com", "Doe"),
+            new Sites("github.com", "Doe"),
+            new Sites("youtrack.cloud", "Smith")
+        };
+        Data = new ObservableCollection<Sites>(data);
+    }
+}
+
+
+public class Sites
+{
+
+    public string Website { get; set; }
+    public string Username { get; set; }
+    public Sites(string website , string username)
+    {
+        Website = website;
+        Username = username;
+    }
 }
