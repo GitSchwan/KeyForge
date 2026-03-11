1d9b3d0c137feefd558cbc92c1001252a94df0a7
GitSchwan <fabianschwanck@hotmail.com>
2026-03-11 11:56:47 +0100

    Enhance LoginView and MainWindow: wrap LoginView content in a border, refine MainWindow appearance with transparency settings, and update navigation to switch to LoginViewModel.

--- KeyForge/Views/MainWindow.axaml.cs
+++ KeyForge/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel;
 using Avalonia.Controls;
+using Avalonia.Media;
+using KeyForge.ViewModels;
 
 namespace KeyForge.Views;
 
@@ -7,5 +10,49 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        Opened += (_, _) => AttachToViewModel();
+        DataContextChanged += (_, _) => AttachToViewModel();
+    }
+
+    private void AttachToViewModel()
+    {
+        if (DataContext is not MainWindowViewModel vm)
+            return;
+
+        vm.PropertyChanged -= MainWindowViewModelOnPropertyChanged;
+        vm.PropertyChanged += MainWindowViewModelOnPropertyChanged;
+
+        UpdateWindowAppearance(vm.CurrentViewModel);
+    }
+
+    private void MainWindowViewModelOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (sender is not MainWindowViewModel vm)
+            return;
+
+        if (e.PropertyName == nameof(MainWindowViewModel.CurrentViewModel))
+        {
+            UpdateWindowAppearance(vm.CurrentViewModel);
+        }
+    }
+
+    private void UpdateWindowAppearance(ViewModelBase? currentViewModel)
+    {
+        if (currentViewModel is LoginViewModel)
+        {
+            TransparencyLevelHint =
+            [
+                WindowTransparencyLevel.AcrylicBlur,
+                WindowTransparencyLevel.Blur,
+                WindowTransparencyLevel.Transparent
+            ];
+            Background = Brushes.Transparent;
+        }
+        else
+        {
+            TransparencyLevelHint = [WindowTransparencyLevel.None];
+            Background = Brushes.Black;
+        }
     }
 }
